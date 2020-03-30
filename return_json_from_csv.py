a471e6415cf953ef0de683609560b48209d92cb0
anastasiia.pyrih <anastasiia.pyrih@refinitiv.com>
2020-03-30 19:03:19 +0300

    little json fix-6

--- return_json_from_csv.py
+++ return_json_from_csv.py
@@ -59,7 +59,7 @@ def upload_file():
 
 def get_json_data(path):
     df = pd.read_csv(path, index_col=None)
-    return json.loads(df.to_json(orient='records'))
+    return json.loads(df.to_json(orient='columns'))
 
 
 def delete_file(path):
