97c00752dba976eedf68b98b098a32b811241716
anastasiia.pyrih <anastasiia.pyrih@refinitiv.com>
2020-03-30 18:53:13 +0300

    little json fix-5

--- return_json_from_csv.py
+++ return_json_from_csv.py
@@ -39,7 +39,6 @@ def upload_file():
 
         if file.filename == '':
             flash('No selected file')
-
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
@@ -47,35 +46,20 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], csv_filename))
             files = [f for f in listdir(app.config['UPLOAD_FOLDER']) if isfile(join(app.config['UPLOAD_FOLDER'], f))]
             json_data = get_json_data(os.path.join(app.config['UPLOAD_FOLDER'], files[0]))
+
             response = Response(headers={'Access-Control-Allow-Origin': '*'})
             response.set_data(json.dumps(json_data))
-            filename_without_extention = get_filename(os.path.join(app.config['UPLOAD_FOLDER'], files[0]))
-            json_filename = filename_without_extention + '.json'
+
             delete_file(os.path.join(app.config['UPLOAD_FOLDER'], csv_filename))
-            delete_file(os.path.join(app.config['UPLOAD_FOLDER'], json_filename))
+
             return json_data
     else:
         return render_template('index.html')
 
 
-def get_filename(path):
-    filename = path.split('/')[-1].split('.')[0]
-    return filename
-
-
 def get_json_data(path):
-
     df = pd.read_csv(path, index_col=None)
-
-    folder_path = "/".join(path.split('/')[:-1])
-    filename = get_filename(path)
-
-    json_path = folder_path + '/' + filename + '.json'
-
-    df.to_json(json_path, index=False)
-
-    with open(json_path) as f:
-        return json.loads(f.read())
+    return json.loads(df.to_json(orient='records'))
 
 
 def delete_file(path):
